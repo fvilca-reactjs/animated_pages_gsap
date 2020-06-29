3187ca4d088d6152a8e7054980b5eeff9eb0a796
fvilca1 <fvilca@gmail.com>
2020-06-29 01:14:48 -0500

    adding mo.js for cool microinteractions

--- src/Pages/hover/hover.jsx
+++ src/Pages/hover/hover.jsx
@@ -9,24 +9,35 @@ function OverEffects() {
     console.log('prev:', localStorage.getItem('prev'))
     console.log('current:', localStorage.getItem('current'))
     useEffect(() => {
-        new mojs.Shape(
-            
-        )
-        return () => {
-            cleanup
-        }
-    }, [input])
+
+        setTimeout(()=>{
+            const animation = new mojs.Burst({
+                parent: '#test',
+                shape: 'circle',
+                fill: {'#999': '#fc46ad'},
+                radius:{20:40},
+                duration:4000,
+                isYoyo: true,
+                isShowStart: true,
+                easing:'elastic.inout',
+                repeat:1
+            })
+            animation.play();
+        },1000)
+       
+        
+    }, [])
     //es importANTE Q ESTE EN UN DIV EN LUGAR Q UN FRAGMENT
     return (
-        <div>
-            <Title
+        <div className='page-container'>
+            <Title 
                 title1='Explosion'
                 title2='...' />
             <p className='page-info'>
                 Pure CSS BigBang Explosion Hover Effects - This Video is for Entertainment Purposes Only
             </p>
-            <div className='dot'></div>
-            <div id="test"></div>
+            <div className='dot'/>
+            <div id="test"/>
         </div>
     )
 }
